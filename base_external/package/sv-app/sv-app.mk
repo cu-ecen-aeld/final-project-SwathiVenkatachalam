e9baa0f70bc483b3c41d2b607a4254e217524ffa
deepak <deepak@Ubuntu.myguest.virtualbox.org>
2024-05-06 11:33:44 -0600

    updated commit for app

--- base_external/package/sv-app/sv-app.mk
+++ base_external/package/sv-app/sv-app.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference camera driver code
-SV_APP_VERSION = '7153bf49f985871cab53c3243a668cfd2739c0e3'
+SV_APP_VERSION = '03b62dc9d3013a2904ecf9bb0e42e90cf3ae030a'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
